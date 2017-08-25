cce4240a37c91fc48c00415284ef40d5ecc15157
周泽宇 <zhouzeyuinchina@gmail.com>
2017-08-25 23:48:19 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
+    member do
+       post :join
+       post :quit
+    end
+
     resources :posts
-  end 
+  end
   root 'groups#index'
 end
